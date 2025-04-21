b3083cdbfa4f8668d8ee19fd671b10e51a987e48
Natan Ayele <sim935160@gmail.com>
2025-04-21 11:15:20 -0700

    Fix config: use env defaults to prevent CI error

--- gig_bot/config.py
+++ gig_bot/config.py
@@ -1,11 +1,8 @@
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 
-
-
-TOKEN = os.environ.get("TELEGRAM_TOKEN")
-DOCUMENTATION_GROUP_ID = int(os.environ.get("DOCUMENTATION_GROUP_ID", -1002531915136))
-MARKETING_GROUP_ID     = int(os.environ.get("MARKETING_GROUP_ID", -1002523694436))
-
-
+DOCUMENTATION_GROUP_ID = int(os.environ.get("DOCUMENTATION_GROUP_ID", "123456"))
+MARKETING_GROUP_ID = int(os.environ.get("MARKETING_GROUP_ID", "654321"))
+TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN", "dummy-token")
